8f3c097ee27ba451c1363afbe5930fdba48ffd12
✅ Eliezer Garcia <71297177+eliezerjg@users.noreply.github.com>
2025-03-31 14:52:33 -0300

    feat: added palindrome algo
    feat: added palindrome algo

---
+++ Algorithms/Panlindrome.ts
@@ -0,0 +1,17 @@
+export default class Palindrome{
+     isPalindrome(word : string) : boolean{
+
+         var palindromeArray : string[] = [];
+        for(let index= word.length - 1; index >= 0; index--){
+            var letter = word[index];
+            palindromeArray.push(letter);
+        }
+
+        return word === result;
+    }
+}
+
+
+const palindrome : Palindrome = new Palindrome();
+console.log(palindrome.isPalindrome("ana"));
+console.log(palindrome.isPalindrome("ovo"));
