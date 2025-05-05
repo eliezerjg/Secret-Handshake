717844b643c4be975db43bc7ab77da01e8f19bc1
✅ Eliezer Garcia <71297177+eliezerjg@users.noreply.github.com>
2025-05-05 18:30:10 -0300

    fix: refactoring to 2 pointers

--- Algorithms/SAP/Water.java
+++ Algorithms/SAP/Water.java
@@ -1,39 +1,32 @@
 package br.com.company.algorithms.techmahindra;
 
 class Water {
-    public static int min(int left, int right){
-        if(left < right){
-            return left;
-        }
-        return right;
-    }
 
     public static int maxArea(int[] height) {
-        int lastMathArea = 0;
-        int lastMathVolumeArea = 0;
+        int maxArea = 0;
+        int left = 0;
+        int right = height.length - 1;
 
-        for(int i = 0; i < height.length; i++){
-            int leftValue = height[i];
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = minHeight * width;
 
-            for(int j = height.length -1; j >= 0; j--){
-                int rightValue = height[j];
-                int xAxysBase = (j+1) - i;
+            maxArea = Math.max(maxArea, area);
 
-                // b x h
-                int mathArea = xAxysBase * (min(leftValue, rightValue));
-                if(lastMathArea < mathArea){
-                    lastMathArea = mathArea;
-                    lastMathVolumeArea = (min(leftValue, rightValue)) * (min(leftValue, rightValue));
-                }
-
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
 
-        return lastMathVolumeArea;
+        return maxArea;
     }
 
     public static void main(String[] args) {
-        System.out.println("expected: 49, " + maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
-        System.out.println("expected: 1, " + maxArea(new int[]{1,1}));
+        System.out.println("expected: 49, actual: " + maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+        System.out.println("expected: 1, actual: " + maxArea(new int[]{1,1}));
+        System.out.println("expected: 2, actual: " + maxArea(new int[]{1,2,1}));
     }
 }
